bdd39d8ed251bf8c8eecc5e6f990bedefc68d1ad
Mirco Tischler <mt-ml@gmx.de>
2015-03-23 10:31:16 +0100

    gnome-shell 3.16 renames Shell.KeyBindingMode to Shell.ActionMode
    In gnome-shell commit e0eebc90e024481a60d41fdd36606a9ff1bb3b8d first
    included in 3.15.3.

--- drop-down-terminal@gs-extensions.zzrough.org/extension.js
+++ drop-down-terminal@gs-extensions.zzrough.org/extension.js
@@ -459,7 +459,11 @@ const DropDownTerminalExtension = new Lang.Class({
     },
 
     _bindShortcut: function() {
-        if (Main.wm.addKeybinding && Shell.KeyBindingMode) // introduced in 3.7.5
+        if (Main.wm.addKeybinding && Shell.ActionMode) // introdued in 3.15.3
+            Main.wm.addKeybinding(REAL_SHORTCUT_SETTING_KEY, this._settings, Meta.KeyBindingFlags.NONE,
+                                  Shell.ActionMode.NORMAL | Shell.ActionMode.MESSAGE_TRAY,
+                                  Lang.bind(this, this._toggle));
+        else if (Main.wm.addKeybinding && Shell.KeyBindingMode) // introduced in 3.7.5
             Main.wm.addKeybinding(REAL_SHORTCUT_SETTING_KEY, this._settings, Meta.KeyBindingFlags.NONE,
                                   Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.MESSAGE_TRAY,
                                   Lang.bind(this, this._toggle));
