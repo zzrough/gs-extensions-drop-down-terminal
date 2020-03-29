2a89bb911fc680af22ecbabc955bb115e2cc4d7d
Pavel Shabardin <bigbn@mail.ru>
2020-03-29 13:04:46 +0400

    [dist]

--- dist/terminal.js
+++ dist/terminal.js
@@ -940,7 +940,7 @@ const DropDownTerminalX = new Lang.Class({
     }
   },
   showMOTR: function (terminal) {
-    const currentVersion = '1.4.5';
+    const currentVersion = '1.4.6';
 
     const lastVersion = this._settings.get_string(MOTR_VERSION_SETTING_KEY).trim();
 
@@ -950,9 +950,9 @@ const DropDownTerminalX = new Lang.Class({
       terminal.feed('▒█▒█▒█ █▄▄█ █▄▄▀ █░░ █▀▀█ 　 ░▄▀ █▄▀█ ░▄▀ █▄▀█ \n\r');
       terminal.feed('▒█░░▒█ ▀░░▀ ▀░▀▀ ▀▀▀ ▀░░▀ 　 █▄▄ █▄▄█ █▄▄ █▄▄█ \n\r');
       terminal.feed('\n\r');
-      terminal.feed('  Release notes for 1.4.5\n\r');
+      terminal.feed('  Release notes for 1.4.6\n\r');
       terminal.feed('\n\r');
-      terminal.feed(' - Gnome 3.34 backward compatibility \n\r');
+      terminal.feed(' - Gnome 3.28 backward compatibility \n\r');
       terminal.feed('\n\r');
 
       this._settings.set_string(MOTR_VERSION_SETTING_KEY, currentVersion);
